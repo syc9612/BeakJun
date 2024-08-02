86ed034b7f5b2a320561be34f432aa2fa5b478c9
syc9612 <syc9612@naver.com>
2024-08-03 00:29:55 +0900

    08/03

--- Polynomial.cpp
+++ Polynomial.cpp
@@ -26,6 +26,24 @@ class Polynomial{
     }  
 
     void add( Polynomial a, Polynomial b){
-        
+        if(a.degree > b.degree){
+            *this = a;
+            for( int i =0; i<=b.degree; i++){
+                coef[i + (degree-b.degree)] += b.coef[i];
+            }
+            
+        }
+        else {
+            *this = b;
+            for(int i=0; i<=a.degree;i++){
+                coef[i+(degree-a.degree)] += a.coef[i];
+            }
+
+            bool isZero(){ return degree == 0;}
+            void negate(){
+                for(int i=0;i<=degree;i++)
+                    coef[i] = -coef[i];
+            }
+        }
     }
 };
