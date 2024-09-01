fa78dec742f8a558246e61780fe58053643c7ef7
syc9612 <syc9612@naver.com>
2024-09-01 20:50:51 +0900

    "09/01'

--- Graph/AdjListGraph.h
+++ Graph/AdjListGraph.h
@@ -4,8 +4,8 @@
 class AdjListGraph{
     protected:
     int size;
-    cahr vertices[MAX_VTXS];
-    Node* adj[MAX_VTXS];
+    cahr vertices[MAX_VTXS]; //information of vertice
+    Node* adj[MAX_VTXS]; //the near list
     
     public:
     AdjListGraph():size(0)P 
@@ -20,4 +20,22 @@ class AdjListGraph{
     bool isEmpty(){return (size == 0);}
     bool isFull(){return (size >= MAX_VTXS);}
     char getVertex(int i){ return vertices[i];}
+
+    void insertVertex( char val){
+        if( !isFull()){
+            vertices[size] = val;
+            adj[size++] = NULL;
+        }
+        else{ printf("ERROR: vertices over")}
+    }
+    void insertEdge(int u, int v){
+        adj[u] = new Node(v, adj[u];)
+        adj[v] = new node(u, adj[v];)
+    }
+    void display(){}
+
+    Node* adjacent(int v){ return adj[v];} //v번째 정점의 인접 정점 리스트를 반환하는 함수. 인정 리스트의 헤더 포인터를 반환한다.
+    
+
+
 }
