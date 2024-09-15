2b1fc47545ece88910a48577eca8e3f2032adbfe
syc9612 <syc9612@naver.com>
2024-09-15 15:33:01 +0900

    "09/15"

--- EmailBook/crud.h
+++ EmailBook/crud.h
@@ -2,37 +2,48 @@
 #include <fstream>
 #include <string>
 
+std::string address_check(std::string address){
+    //if @ is not existed, error
+    std::cout << address.find("@");
+    if(addess.find("@") == string::npos){
+        std::cout <<"no @ in Email."<<std::endl;
+    }
+    else{
+        std::cout <<"check done" << std::endl;
+    }
+}
+
 char enroll_nm_adr(char name, char address){
     //sign in
-    std::string line;
-    ifstream file("user_info.txt"); //make file and create
+    //https://m.blog.naver.com/sea5727/220978963342
     
-    if(file.is_open()){
-        std::file << name << "\t";
-        std::file << address << std::endl;;
-    }
-    else{
-        std::cout << "error" << std::endl;
-        return 1;
-    }
+    std::fsteram dataFile;
+    std::string buffer;
 
+    dataFile.open("user_info.txt", ios::out);
+    std::dataFile << "new word in file" << std::endl;
+    dataFile.write("12345",5);
+    dataFile.close();
+    dataFile.open("user_info.txt", ios::in);
 
-    if(file.is_open()){
-        while(getline(file, line)){
-            std::cout << line << std::endl;
-        }
-        file.close();
-    }
-    else{
-        std::cout << "Unable to open file";
-        return 1;
-    }
-    
+    getline(dataFile, buffer);
+    std::cout << vuffer << std::endl;
+    char charbuff[10] = {0}; //initialize
+    dataFile.read(charbuff,5);
+    std::cout<<charbuff<<std::endl;
+    dataFile.close();
+
     //duplicate check
 
 }
 
 void print(char trg_name){
+    //print all
+    if(tar_name == NULL)
 
+    //print target
+    else(
+
+    )
 }
 
